09490ff051dce7d7241f572a547e90a80b05e908
SP4567 <suyashpandey668@gmail.com>
2025-07-31 10:33:32 +0530

    Initial commit: Add E-commerce App source code and database scripts

---
+++ Desktop/Application/E-commerce_App/src/Database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+    products (
+        id INTEGER PRIMARY KEY,
+        cost REAL,
+        category TEXT,
+        name TEXT,
+        brand TEXT,
+        retail_price REAL,
+        department TEXT,
+        sku TEXT UNIQUE,
+        distribution_center_id INTEGER
+    );
